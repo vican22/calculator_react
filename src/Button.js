dab8e32e7d5b15b29a543444de05f4c5fc563ef1
Vedran Knezevic <vedranknezevic1@yahoo.com>
2018-10-04 15:59:57 +0200

    React Calculator ver1.1

--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,12 @@
 import React, { Component } from "react";
 
-const style = {
-  display: "inline-block",
-  color: "#000",
-  border: "1px solid black",
-  backgroundColor: "rgba(172,45,56, 0.7)",
-  margin: "10px",
-  padding: "10px",
-  borderRadius: "50%",
-  fontSize: "20px",
-  height: "25px",
-  width: "25px"
-};
+import "./Button.css";
 
 class Button extends Component {
   render() {
     return (
       <div
-        style={style}
+        className={"Button"}
         onClick={this.props.onClick}
         data-value={this.props.value}
       >
